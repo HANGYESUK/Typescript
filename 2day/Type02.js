825773f4436c6a9ae6cd3690921129b55d4aeb36
한규석 <78546881+HANGYESUK@users.noreply.github.com>
2022-02-11 09:05:13 +0900

    Add files via upload

---
+++ 2day/Type02.js
@@ -0,0 +1,25 @@
+// void타입 - 리턴 막아줌
+// 타입 지정된 파라미터는 필수(파라미터 옆에 ? 넣어면 옵션으로 변경됨)
+// ?는 undifined로 타입 추가
+// 엄근진 이라 타입 union으로 지정하면 계산하는 연산 안됨 (숫자 + 숫자)만 가능
+function go(x) {
+    console.log(x);
+}
+go(2);
+//숙제1 
+function Name(n) {
+    if (n) {
+        console.log("안녕하세요" + n);
+    }
+    else {
+        console.log("이름이 없습니다");
+    }
+}
+Name("홍길동");
+//숙제2
+function Map(n) {
+    return n.toString().length;
+}
+Map(345);
+Map("2345");
+//숙제3
