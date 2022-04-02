ff5fac74cbb4ae297d5b4afb62b176fba70be337
HANGYESUK <akakakak66@naver.com>
2022-04-02 17:36:03 +0900

    tsx - 타입스크립트 리액트에서 사용

---
+++ react_tsx/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let box :JSX.IntrinsicElements['div']= <div>안녕 세상</div>;
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      { box }
+      <Profile name ="Kim" age={25}></Profile>
+    </div>
+  );
+}
+
+//컴포넌트 타입지정 - 파라미터(props), return값
+function Profile(props :{name :string, age? :number}) :JSX.Element {
+  return (
+    <div>
+      { props.name } 프로필 입니다.
+      <br/>
+      { props.age } 살 입니다.
+    </div>
+  )
+}
+
+export default App;
